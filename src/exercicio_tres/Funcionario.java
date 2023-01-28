25a08cd2fbbbad859aa73331c6fb1e33e5427a30
samuel-n-silva <samuel_nunes-silva@estudante.sesisenai.org.br>
2023-01-27 21:01:53 -0300

    Exercicio 3

---
+++ src/exercicio_tres/Funcionario.java
@@ -0,0 +1,13 @@
+package exercicio_tres;
+
+public class Funcionario {
+    public String nome;
+    public String endereco;
+    public double salario;
+
+    public Funcionario(String nome, String endereco, double salario) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.salario = salario;
+    }
+}
---
+++ src/exercicio_tres/Main.java
@@ -0,0 +1,10 @@
+package exercicio_tres;
+public class Main {
+    public static void main(String[] args) {
+        Funcionario Samuel = new Funcionario("Samuel","Rua lagoa",1800);
+
+        System.out.println(Samuel.nome);
+        System.out.println(Samuel.endereco);
+        System.out.println(Samuel.salario);
+    }
+}
